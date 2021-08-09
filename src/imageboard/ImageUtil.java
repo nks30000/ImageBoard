6550a1242d709f79197df4f2b91e79d37c06031d
luvoa <luvoa@Aubrey>
2021-08-09 18:02:44 +0900

    받아라

---
+++ src/imageboard/ImageUtil.java
@@ -0,0 +1,15 @@
+package imageboard;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageUtil {
+
+}
